fca852e7e39fb344bebec9446542a0a0538b380c
vasilevieru <vasilevieru@mail.ru>
2021-02-20 17:51:44 +0200

    Add some functionality

---
+++ AccessInsideBuildingSystem.Core/Domain/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace AccessInsideBuildingSystem.Core.Domain
+{
+    public class Address : BaseEntity
+    {
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
